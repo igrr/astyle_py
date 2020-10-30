414de27d4503e8230335709bf7ad5c986c600e06
Ivan Grokhotkov <ivan@espressif.com>
2020-10-31 00:38:05 +0100

    Implement more options, expand readme

--- README.md
+++ README.md
@@ -6,3 +6,53 @@ This project wraps `astyle` in a Python package, compatible with [pre-commit](ht
 It allows you to fix source code formatting with a Git pre-commit hook.
 The main motivation for using this Python wrapper, rather than native `astyle` binaries, is that users of all operating systems will be using exactly the same version of astyle. This prevents formatting differences which sometimes occur between different versions of astyle.
 
+To simplify distribution of astyle, it is compiled to WebAssembly ([astyle_py/libastyle.wasm](astyle_py/libastyle.wasm)) and executed using wasmtime runtime via its Python bindings. This package should work on all operating systems supported by wasmtime — at the time of writing these are the x86_64 versions of Windows, Linux, macOS, and aarch64 Linux.
+
+## Usage
+
+By default, `astyle_py` formats the files specified, according to the given options.
+
+### With pre-commit
+
+1. Set up pre-commit for your project as described in https://pre-commit.com/#install.
+2. Add `astyle_py` to your `.pre-commit-config.yaml` file:
+   ```yaml
+   repos:
+   -   repo: https://github.com/igrr/astyle_py.git
+       rev: master
+       hooks:
+       -   id: astyle_py
+           args: [--style=linux]
+   ```
+
+   Place the required astyle formatting options to the `args` array. See the next section for details.
+
+   If necessary, add `verbose: true` to see the output.
+
+   Use `files:` option to configure the regex pattern used to match the files to be formatted. Default is `'^.*\.(c|cpp|cxx|h|hpp|inc)$'`.
+
+### From command line
+
+There is no PyPI release yet, but you can install the package directly from Github:
+```
+pip install git+https://github.com/igrr/astyle_py.git
+```
+
+Usage:
+```
+astyle_py [options] <files>
+```
+
+`<files>` — list of files to process
+
+`[options]` — astyle formatting options, see http://astyle.sourceforge.net/astyle.html for reference.
+
+Note that __only a few__ of the options in ["Other options"](http://astyle.sourceforge.net/astyle.html#_Other_Options) and ["Command Line Only"](http://astyle.sourceforge.net/astyle.html#_Command_Line_Only) are supported, namely:
+
+* `--options=<file>` — read more options from the specified file. Empty lines and lines starting with `#` are ignored.
+* `--dry-run` — don't format the files, return non-zero exit code if any file would change after formatting.
+* `--exclude=<pattern>` — skip files matching the given pattern. Note that patterns use the syntax of [Gitlab CODEOWNERS files](https://docs.gitlab.com/ee/user/project/code_owners.html#the-syntax-of-code-owners-files).
+* `--exclude-list=<file>` — skip files matching the list of patterns specified in a file.
+* `--version` — print the version and exit.
+* `--quiet` — don't print diagnostic messages; by default the files containing errors or files which are formatted are listed in stderr.
+
--- astyle_py/astyle_wrapper.py
+++ astyle_py/astyle_wrapper.py
@@ -1,7 +1,19 @@
 import os
+import re
 import sys
-from wasmtime import Engine, ValType, WasiInstance, Store, \
-    Linker, Module, Func, FuncType, Config, WasiConfig, ExitTrap
+from wasmtime import (
+    Engine,
+    ValType,
+    WasiInstance,
+    Store,
+    Linker,
+    Module,
+    Func,
+    FuncType,
+    Config,
+    WasiConfig,
+)
+from . import __version__
 
 
 class WasmString:
@@ -47,40 +59,183 @@ class WasmString:
         return WasmString(inst, addr, n_bytes, string)
 
 
+class Astyle:
+    def __init__(self):
+        wasm_cfg = Config()
+        wasm_cfg.cache = True
+        store = Store(Engine(wasm_cfg))
+        linker = Linker(store)
+
+        wasi_cfg = WasiConfig()
+        wasi_inst = WasiInstance(store, "wasi_snapshot_preview1", wasi_cfg)
+        linker.define_wasi(wasi_inst)
+
+        self.inst = None
+        err_handler_type = FuncType([ValType.i32(), ValType.i32()], [])
+        err_handler_func = Func(store, err_handler_type, self._err_handler)
+        linker.define("env", "AStyleErrorHandler", err_handler_func)
+
+        wasm_file = os.path.join(os.path.dirname(__file__), "libastyle.wasm")
+        module = Module.from_file(store.engine, wasm_file)
+        self.inst = linker.instantiate(module)
+        self.inst.exports["_initialize"]()
+
+        self._opts_ptr = WasmString.from_str(self.inst, "")
+
+    def version(self):
+        res_addr = self.inst.exports["AStyleGetVersion"]()
+        return str(WasmString.from_addr(self.inst, res_addr))
+
+    def set_options(self, options):
+        self._opts_ptr = WasmString.from_str(self.inst, options)
+
+    def format(self, source):
+        src_ptr = WasmString.from_str(self.inst, source)
+        fmt_func = self.inst.exports["AStyleWrapper"]
+        res_addr = fmt_func(src_ptr.addr, self._opts_ptr.addr)
+        return str(WasmString.from_addr(self.inst, res_addr))
+
+    def _err_handler(self, errno, errptr):
+        errstr = WasmString.from_addr(self.inst, errptr)
+        print("error: {} ({})".format(errstr, errno), file=sys.stderr)
+        raise SystemExit(1)
+
+
+def get_lines_from_file(fname):
+    with open(fname) as f:
+        return [line.strip() for line in f if not line.startswith("#") and len(line.strip()) > 0]
+
+
+def pattern_to_regex(pattern):
+    """
+    Convert the CODEOWNERS-style path pattern into a regular expression string
+    """
+    orig_pattern = pattern  # for printing errors later
+
+    # Replicates the logic from normalize_pattern function in Gitlab ee/lib/gitlab/code_owners/file.rb:
+    if not pattern.startswith('/'):
+        pattern = '/**/' + pattern
+    if pattern.endswith('/'):
+        pattern = pattern + '**/*'
+
+    # Convert the glob pattern into a regular expression:
+    # first into intermediate tokens
+    pattern = (pattern.replace('**/', ':REGLOB:')
+                      .replace('**', ':INVALID:')
+                      .replace('*', ':GLOB:')
+                      .replace('.', ':DOT:')
+                      .replace('?', ':ANY:'))
+
+    if pattern.find(':INVALID:') >= 0:
+        raise ValueError("Likely invalid pattern '{}': '**' should be followed by '/'".format(orig_pattern))
+
+    # then into the final regex pattern:
+    re_pattern = (pattern.replace(':REGLOB:', '(?:.*/)?')
+                         .replace(':GLOB:', '[^/]*')
+                         .replace(':DOT:', '[.]')
+                         .replace(':ANY:', '.') + '$')
+    if re_pattern.startswith('/'):
+        re_pattern = '^' + re_pattern
+
+    return re_pattern
+
+
+def file_excluded(fname, exclude_regexes):
+    for regex in exclude_regexes:
+        if re.search(regex, "/" + fname):
+            return True
+    return False
+
+
 def main():
-    wasm_cfg = Config()
-    wasm_cfg.cache = True
-    store = Store(Engine(wasm_cfg))
-    linker = Linker(store)
+    if "--version" in sys.argv:
+        print(
+            "astyle_py v{} with astyle v{}".format(
+                __version__, Astyle().version()
+            )
+        )
+        raise SystemExit(0)
 
-    wasi_cfg = WasiConfig()
-    inst = WasiInstance(store, "wasi_snapshot_preview1", wasi_cfg)
-    linker.define_wasi(inst)
+    for i, arg in enumerate(sys.argv[1:], 1):
+        if not arg.startswith("--"):
+            break
 
-    inst = None
+    options = sys.argv[1:i]
+    files = sys.argv[i:]
+    exclude_list = []
+    fix_formatting = True
+    quiet = False
+    options_to_remove = []
 
-    def error_handler(errno, errptr):
-        errstr = WasmString.from_addr(inst, errptr)
-        print("error: %s (%d)" % (errno, errstr), file=sys.stderr)
-        sys.exit(1)
+    for o in options:
+        o_trimmed = o[2:] if o.startswith("--") else o
+        parts = o_trimmed.split("=")
+        opt = parts[0]
+        value = parts[1] if len(parts) > 1 else None
 
-    error_handler_func = Func(store, FuncType([ValType.i32(), ValType.i32()], []), error_handler)
-    linker.define("env", "AStyleErrorHandler", error_handler_func)
+        def ensure_value():
+            if not value:
+                print("Option {} requires a value".format(opt), file=sys.stderr)
+                raise SystemExit(1)
 
-    wasm_file = os.path.join(os.path.dirname(__file__), 'libastyle.wasm')
-    module = Module.from_file(store.engine, wasm_file)
-    inst = linker.instantiate(module)
+        if opt == "dry-run":
+            options_to_remove.append(o)
+            fix_formatting = False
 
-    src = WasmString.from_str(inst, "int foo(void)\n{ int x = 2; return 32; }")
-    opts = WasmString.from_str(inst, "--style=otbs")
+        elif opt == "options":
+            options_to_remove.append(o)
+            ensure_value()
+            options += get_lines_from_file(value)
 
-    try:
-        res_addr = inst.exports["AStyleGetVersion"]()
-        print("Astyle version %s" % WasmString.from_addr(inst, res_addr))
+        elif opt == "exclude":
+            options_to_remove.append(o)
+            ensure_value()
+            exclude_list.append(value)
 
-        inst.exports["_initialize"]()
-        res_addr = inst.exports["AStyleWrapper"](src.addr, opts.addr)
-        print(WasmString.from_addr(inst, res_addr))
-    except ExitTrap as trap:
-        sys.exit(trap.code)
+        elif opt == "exclude-list":
+            options_to_remove.append(o)
+            ensure_value()
+            exclude_list += get_lines_from_file(value)
 
+        elif opt == "quiet":
+            options_to_remove.append(o)
+            quiet = True
+
+    for o in options_to_remove:
+        options.remove(o)
+
+    exclude_regexes = [re.compile(pattern_to_regex(p)) for p in exclude_list]
+
+    astyle = Astyle()
+    astyle.set_options(" ".join(options))
+
+    def diag(*args):
+        if not quiet:
+            print(*args, file=sys.stderr)
+
+    files_with_errors = 0
+    files_formatted = 0
+    for fname in files:
+        if file_excluded(fname, exclude_regexes):
+            diag("Skipping {}".format(fname))
+            continue
+        with open(fname) as f:
+            original = f.read()
+        formatted = astyle.format(original)
+        if formatted != original:
+            if fix_formatting:
+                diag("Formatting {}".format(fname))
+                with open(fname, "w") as f:
+                    f.write(formatted)
+                files_formatted += 1
+            else:
+                diag("Formatting error in {}".format(fname))
+                files_with_errors += 1
+
+    if fix_formatting:
+        if files_formatted:
+            diag("Formatted {} files".format(files_formatted))
+    else:
+        if files_with_errors:
+            diag("Formatting errors found in {} files".format(files_with_errors))
+            raise SystemExit(1)
