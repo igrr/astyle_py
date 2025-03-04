738276da8a637fee3e95be57e8541f4643e43781
Ivan Grokhotkov <ivan@espressif.com>
2025-03-04 15:25:55 +0100

    refactor: switch to pyproject.toml for configuration

--- astyle_py/__init__.py
+++ astyle_py/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2022 Ivan Grokhotkov <ivan@igrr.me>
 # SPDX-License-Identifier: MIT
 from .astyle_wrapper import Astyle, AstyleError
+from .version import __version__
 
-__all__ = ['Astyle', 'AstyleError']
-__version__ = '1.0.5'
+__all__ = ['Astyle', 'AstyleError', '__version__']
